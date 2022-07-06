b4d67f50c83692ead3bc8c2b59602d3da194d4a2
Jose Pedro Dominguez <78109809+JosePedroDominguez@users.noreply.github.com>
2022-07-06 17:12:16 -0300

    Add files via upload

---
+++ js/formulario.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("contacto").addEventListener('submit', validarFormulario); 
+});
+function validarFormulario(evento) {
+    evento.preventDefault();
+    let nombre = document.getElementById('name').value;
+    if(nombre.length > 50) {
+        Swal.fire({
+            title: 'Error en el nombre',
+            text: 'Puede contener un máximo de 50 caracteres',
+            icon: 'error',
+            confirmButtonText: 'Continuar'
+          })
+      return;
+    }
+    let asunto = document.getElementById("asunto").value;
+    if(asunto.length > 50) {
+        Swal.fire({
+            title: 'Error en el asunto',
+            text: 'Puede contener un máximo de 50 caracteres',
+            icon: 'error',
+            confirmButtonText: 'Continuar'
+          })
+      return;
+    }
+    let mensaje = document.getElementById("mensaje").value;
+    if(mensaje.length > 300) {
+        Swal.fire({
+            title: 'Error en el mensaje',
+            text: 'Puede contener un máximo de 300 caracteres',
+            icon: 'error',
+            confirmButtonText: 'Continuar'
+          })
+      return;
+    }
+    this.submit();
+}
